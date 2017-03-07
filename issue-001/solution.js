c1198bbb7358f7a4955e2b058238d5c7efc23ae1
FelixIvance <felixrunye@gmail.com>
2017-03-07 20:56:52 +0300

    solution to drill 1.1

--- issue-001/solution.js
+++ issue-001/solution.js
@@ -1,3 +1,29 @@
 function dataTypeCheck(a) {
     // implement your solution here
+     if (a === +a && a % 1 === 0 ) {
+	 	console.log("its an integer"); //log the result true
+        return (a / 2);
+    } else if (a === +a && a % 1 !== 0 ) {
+    	console.log("its a float");
+        return a * 2;
+    } else if (typeof a === "string" && isNaN(Number(a)) === false) {
+    	console.log("its a string");
+        return Number(a);
+        
+    } else if (typeof a === "string") {
+    	console.log("Any other string");
+    	var ans= "hello "+a;
+    	console.log(ans) ;
+        return "hello " + a;
+    } else if (typeof a !== "string" && isNaN(a) === true) {
+    	console.log("Nan");
+        return 0;
+    } else if (a === null) {
+    	console.log("Null");
+        return 0;
+    } else if (a === undefined) {
+    	console.log("undefined");
+        return false;
+    }
+
 }
