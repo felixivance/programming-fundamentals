d3922c2d16b8ce1c23deebcb22059841675db43b
Felix Ivance <felixivance@users.noreply.github.com>
2017-03-29 17:17:32 +0300

    Drill 2.1: Create Object

---
+++ solution.js
@@ -0,0 +1,9 @@
+// implement your solution here
+function createObject(A) {
+    var obj = {};
+    A.forEach(function(item) {
+        obj[item[0]] = item[1];
+    });
+    return obj; //returns object
+}
+
